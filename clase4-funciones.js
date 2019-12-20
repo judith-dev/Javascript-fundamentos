41d32fc98a8a3d9c04711c3765d4256f3cf13a1b
SAJU <saju@praxis.com.mx>
2019-12-20 12:11:57 -0600

    clase 4

---
+++ clase4-funciones.js
@@ -0,0 +1,10 @@
+var nombre  = "sasha", edad =  28;
+
+function imprimirEdad(n, e) {
+    console.log(`${n} tiene ${e} años`);
+}
+
+imprimirEdad(nombre,edad);
+imprimirEdad('Vicky', 28);
+imprimirEdad('Eric',24);
+imprimirEdad('Dario',27);
