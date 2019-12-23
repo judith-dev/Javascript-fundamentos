2353a96aae2b4a69af092972296f90b7808b3306
SAJU <saju@praxis.com.mx>
2019-12-23 17:03:36 -0600

    clase 19

--- clase19-transformar un array.js
+++ clase19-transformar un array.js
@@ -1,32 +1,36 @@
 var Alvaro = {
    nombre: 'Alvaro',
-   altura: 1.50
+   altura: 1.50,
+   cantidadLibros: 60
  }
  var Daniel = {
    nombre : 'Daniel',
-   altura:  1.40
+   altura:  1.40,
+   cantidadLibros: 12
  }
  var Alexys = {
    nombre: 'Alexys',
-   altura:  1.90
+   altura:  1.90,
+   cantidadLibros: 70
  }
  var Rafa = {
    nombre: 'Rafa',
-   altura:  1.60
+   altura:  1.60,
+   cantidadLibros: 10
  }
 var Alejandro =  {
    nombre: 'Alejandro',
-   altura:  1.75
+   altura:  1.75,
+   cantidadLibros: 30
  }
  var Sofia = {
    nombre: 'Sofia',
-   altura:  1.89
+   altura:  1.89,
+   cantidadLibros: 3
  }
 
 var personas = [Alvaro, Daniel, Alexys,Rafa, Alejandro, Sofia];
 
-var personasCms  =  personas.map((personas)=>({
- ...personas,
- altura: personas.altura * 100
-}))
-console.log(personasCms);
+var totalLibros =  personas.reduce((total,{cantidadLibros}) =>  total + cantidadLibros,0)
+
+console.log(totalLibros);
