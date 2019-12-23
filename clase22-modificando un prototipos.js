f4cc6da7d886b8a2abe452919dc2eea66fe07c28
SAJU <saju@praxis.com.mx>
2019-12-23 17:46:01 -0600

    clase 22

---
+++ clase22-modificando un prototipos.js
@@ -0,0 +1,20 @@
+function persona(nombre, apellido, altura) {
+  this.nombre = nombre;
+  this.apellido = apellido;
+  this.altura =  altura;
+}
+
+const ALTO =  1.8
+
+persona.prototype.saludar = () => console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`);
+
+persona.prototype.soyalto = () => console.log(this.altura >= ALTO ? 'Soy Alta' : 'Soy Chaparra');
+
+var judith  = new persona('judith', 'sanchez', 1.50);
+judith.saludar();
+judith.soyalto();
+
+var erica  = new persona('erica', 'luna', 1.80);
+erica.saludar();
+erica.soyalto();
+
