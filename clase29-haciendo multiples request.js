9ee1788104f36c57e64b1685b41f2674101213b3
SAJU <saju@praxis.com.mx>
2019-12-30 15:06:25 -0600

    add clase 29

--- clase29-haciendo multiples request.js
+++ clase29-haciendo multiples request.js
@@ -1,10 +1,7 @@
 const API = 'https://swapi.co/api/';
 const PEOPLE_URL = 'people/:id';
 
+const luke = `${API}${PEOPLE_URL.replace(':id', 1)}`;
 const opts = { crossDomain: true };
 const onPeopleResponse = (people) => console.log(`Hola yo soy ${people.name}`);
-const obtenerPersonaje = (id) => {
-    const url  = `${API}${PEOPLE_URL.replace(':id', id)}`;
-    $.get(url, opts, onPeopleResponse)
-}
-obtenerPersonaje(40);
+$.get(luke, opts, onPeopleResponse)
